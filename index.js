8f264c1314187db38737bb28807da36dd682e5b4
SongFuZhen <evil_song@126.com>
2022-05-12 15:53:33 +0800

    2 files modified, 2 added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // run `node index.js` in the terminal
 
 // 使用 require 引入
+// CommonJS模块的加载机制是，输入的是被输出的值的拷贝。也就是说，一旦输出一个值，模块内部的变化就影响不到这个值。
 const modules_demo = require('./modules/demo');
 
 console.log('初始化 Number', modules_demo.number);
