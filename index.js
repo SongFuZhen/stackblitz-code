768333084595808861c7926000bfa918a6a0bb3f
SongFuZhen <evil_song@126.com>
2022-05-12 08:53:53 +0800

    init node code

---
+++ index.js
@@ -0,0 +1,6 @@
+// run `node index.js` in the terminal
+
+const modules_demo = require('./modules/demo');
+
+console.log('arr', modules_demo.arr);
+console.log('foo', modules_demo.foo());
---
+++ modules/demo.js
@@ -0,0 +1,7 @@
+exports.arr = [1, 2, 3, 4, 5];
+
+exports.foo = function () {
+  console.log('--- demo.js foo() --- ');
+
+  return false;
+};
