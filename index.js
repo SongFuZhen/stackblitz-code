42d240a2b6861273dadc6469979e91aafef77d3d
SongFuZhen <evil_song@126.com>
2022-05-12 15:44:35 +0800

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // run `node index.js` in the terminal
 
+// 使用 require 引入
 const modules_demo = require('./modules/demo');
 
-console.log('arr', modules_demo.arr);
-console.log('foo', modules_demo.foo());
+console.log('初始化 Number', modules_demo.number);
+console.log('初始化 Arr', modules_demo.arr);
+console.log('-------------------------------');
+console.log('函数操作', modules_demo.foo());
+console.log('-------------------------------');
+console.log('转化后 Number', modules_demo.number);
+console.log('转化后 Arr', modules_demo.arr);
--- modules/demo.js
+++ modules/demo.js
@@ -1,7 +1,24 @@
-exports.arr = [1, 2, 3, 4, 5];
+// CommonJS 的写法
+// 使用 Module.exports 可以导出任何时值
+// number 是原始类型， number 值在经过 Foo 调用之后，内部会改变，但是再外部调用是不会改变的
+// arr 是对象， arr 值会改变
+var number = 5;
 
-exports.foo = function () {
+var arr = [1, 2, 3, 4, 5];
+
+var foo = function () {
   console.log('--- demo.js foo() --- ');
+  // 操作 number
+  number = number + 1;
+  console.log('new Number :: ', number);
+
+  // 操作 arr
+  arr.push(6);
+  console.log('new Arr :: ', arr);
 
   return false;
 };
+
+module.exports.number = number;
+module.exports.arr = arr;
+module.exports.foo = foo;
