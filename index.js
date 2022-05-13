c8a8b88f347f270802fa155b89bab1ab76ae0cbc
SongFuZhen <evil_song@126.com>
2022-05-13 10:01:58 +0800

    1 file modified

--- index.js
+++ index.js
@@ -17,3 +17,4 @@ console.log('函数操作', modules_demo.foo());
 console.log('-------------------------------');
 console.log('转化后 Number', modules_demo.number);
 console.log('转化后 Arr', modules_demo.arr);
+
