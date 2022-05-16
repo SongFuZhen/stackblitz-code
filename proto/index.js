19426c84e076e40476c3a949bf073460d5e11f46
SongFuZhen <evil_song@126.com>
2022-05-16 15:22:51 +0800

    1 file modified

--- proto/index.js
+++ proto/index.js
@@ -48,3 +48,52 @@ console.log(
 );
 
 console.log('Object 的原型的原型 = null', Object.prototype.__proto__ === null);
+
+console.log('-------------- 原型的相等 --------------');
+
+const equal_arr = [
+  ['f1.__proto__', 'func.prototype'],
+  ['func.prototype', 'func.prototype'],
+  ['func.prototype.constructor', 'func'],
+  ['func.__proto__', 'Function.prototype'],
+  ['func.prototype.__proto__', 'Object.prototype'],
+
+  ['Object.prototype', 'Object.prototype'],
+  ['Object.prototype.constructor', 'Object'],
+  ['Object.__proto__', 'Function.prototype'],
+
+  ['Function.prototype', 'Function.prototype'],
+  ['Function.prototype.constructor', 'Function'],
+  ['Function.__proto__', 'Function.prototype'],
+
+  ['Function.prototype.__proto__', 'Object.prototype'],
+  ['Object.prototype.__proto__', 'null'],
+];
+
+for (let i = 0; i < equal_arr.length; i++) {
+  const a = equal_arr[i][0];
+  const b = equal_arr[i][1];
+
+  console.log(`${i + 1}. ${a} === ${b} :: `, eval(a) === eval(b));
+}
+
+console.log('-------------- f1 的原型链 --------------');
+console.log('f1.__proto__ -> func.prototype -> Object.prototype -> null');
+
+console.log(
+  'f1.__proto__ === func.prototype ->',
+  'func.prototype.__proto__ === Object.prototype ->',
+  'Object.prototype.__proto__ === null ->',
+  'null'
+);
+
+console.log('-------------- func 的原型链 --------------');
+
+console.log('func.__proto__ -> Function.prototype -> Object.prototype -> null');
+
+console.log(
+  'func.__proto__ === Function.prototype ->',
+  'Function.prototype.__proto__ === Object.prototype ->',
+  'Object.prototype.__proto__ === null ->',
+  'null'
+);
