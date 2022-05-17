a74ffaac00c024a08e0a05e5e20e7672dcd17ece
SongFuZhen <evil_song@126.com>
2022-05-18 07:51:18 +0800

    1 file modified

--- this/index.js
+++ this/index.js
@@ -1,2 +1,13 @@
+// 'use strict';
+
 // This
 console.log('-------------- This --------------');
+
+(function () {
+  var a = 10;
+  function foo() {
+    console.log(this.a);
+  }
+
+  foo();
+})();
