e1bb077bdfbd9ac1a5290d3912b4af432f0b25f0
SongFuZhen <evil_song@126.com>
2022-05-18 08:14:06 +0800

    1 file modified

--- this/index.js
+++ this/index.js
@@ -1,8 +1,11 @@
 // 'use strict';
 
 // This
-console.log('-------------- This --------------');
+console.log(
+  '-------------- This, 最好使用 VSCode 进行运行，StackBlitz 会自动包裹一个 function, 导致 this 指向不准确 --------------'
+);
 
+// Case 1-1
 (function () {
   var a = 10;
   function foo() {
@@ -10,4 +13,33 @@ console.log('-------------- This --------------');
   }
 
   foo();
-})();
+});
+
+// Case 1-2
+// 'use strict';
+(function () {
+  var a = 10;
+  function foo() {
+    console.log('this1', this);
+    console.log(window.a);
+    console.log(this.a);
+  }
+
+  console.log(window.foo);
+  console.log('this2', this);
+
+  foo();
+});
+
+// Case 1-3
+let a = 10;
+const b = 20;
+
+// function foo() {
+//   console.log(this.a);
+//   console.log(this.b);
+// }
+
+// foo();
+
+console.log(window.a);
