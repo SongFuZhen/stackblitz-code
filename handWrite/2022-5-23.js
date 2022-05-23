ea42e9fdecda1dc205ff6ba91a9e8f1f2975ced4
SongFuZhen <evil_song@126.com>
2022-05-23 11:27:41 +0800

    1 file modified

--- handWrite/2022-5-23.js
+++ handWrite/2022-5-23.js
@@ -1,6 +1,72 @@
 // 手写
 console.log('2022-5-23');
 
+// #region Promise 的应用
+
+// Case1 使用 Promise 实现每隔 1s 输出 1，2，3
+const run = (arr) => {
+  arr.reduce((p, x) => {
+    return p.then(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          console.log(x, new Date().getSeconds());
+          resolve(x);
+        }, 1000);
+      });
+    });
+  }, Promise.resolve());
+};
+
+(function () {
+  const arr = [1, 2, 3];
+  run(arr);
+});
+
+// Case2: 使用Promise实现红绿灯交替重复亮, 红灯3秒亮一次，黄灯2秒亮一次，绿灯1秒亮一次
+
+function red() {
+  console.log('red', new Date().getSeconds());
+}
+
+function green() {
+  console.log('green', new Date().getSeconds());
+}
+
+function yellow() {
+  console.log('yellow', new Date().getSeconds());
+}
+
+// 亮灯
+const light = (timer, cb) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(cb());
+    }, timer);
+  });
+};
+
+const step = () => {
+  Promise.resolve()
+    .then(() => {
+      return light(3000, red);
+    })
+    .then(() => {
+      return light(2000, yellow);
+    })
+    .then(() => {
+      return light(1000, green);
+    })
+    .then(() => {
+      step();
+    });
+};
+
+(function () {
+  step();
+})();
+
+// #endregion
+
 // #region 深拷贝
 
 const deep_clone = (obj, map = new Map()) => {
