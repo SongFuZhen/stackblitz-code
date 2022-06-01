a8bc5019298989dcd14ec02ce97c05c54dd92fa5
SongFuZhen <evil_song@126.com>
2022-06-01 08:41:02 +0800

    1 file modified

--- handWrite/throttle.js
+++ handWrite/throttle.js
@@ -1,6 +1,6 @@
 console.log('Throttle');
 
-// 使用时间戳 进行执行
+// 使用时间戳 进行执行，有头没尾
 function throttle(func, timer) {
   let context;
   let args;
@@ -28,7 +28,7 @@ function func() {
 
 // container.onmousemove = throttle(func, 5000);
 
-// 使用定时器 进行执行
+// 使用定时器 进行执行，没头有尾
 function throttle_timeout(func, timer) {
   let timeout, context, args;
 
@@ -48,37 +48,61 @@ function throttle_timeout(func, timer) {
 // container.onmousemove = throttle_timeout(func, 5000);
 
 // 有头有尾的执行
-function throttle_all(func, timer) {
+function throttle_all(func, timer, options = {}) {
   let timeout, context, args;
   let previous = 0;
 
   const later = function () {
-    previous = +new Date();
-    timeout = null;
+    previous = options.leading ? +new Date() : 0;
+    timeout = undefined;
     func.apply(context, args);
+
+    if (!timeout) {
+      context = args = null;
+    }
   };
 
   const throttle = function () {
-    const now = +new Date();
-    const remaining = timer - (now - previous);
-
     context = this;
     args = arguments;
+
+    const now = +new Date();
+
+    // 无头
+    if (!previous && !options.leading) {
+      previous = now;
+    }
+
+    const remaining = timer - (now - previous);
 
     if (remaining <= 0 || remaining > timer) {
       if (timeout) {
         clearTimeout(timeout);
-        timeout = null;
+        timeout = undefined;
       }
 
       previous = now;
       func.apply(context, args);
-    } else if (!timeout) {
+
+      if (!timeout) {
+        context = args = null;
+      }
+    } else if (!timeout && options.trailing) {
+      // 有尾
       timeout = setTimeout(later, remaining);
     }
+  };
+
+  throttle.cancel = function () {
+    clearTimeout(timeout);
+    previous = 0;
+    timeout = undefined;
   };
 
   return throttle;
 }
 
-container.onmousemove = throttle_all(func, 5000);
+container.onmousemove = throttle_all(func, 2000, {
+  leading: true,
+  trailing: false,
+});
