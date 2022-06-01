137ebf07b4545cda8c27ae6c51aec6c4dcf8e660
SongFuZhen <evil_song@126.com>
2022-06-01 15:49:54 +0800

    1 file modified

--- handWrite/flat_arr.js
+++ handWrite/flat_arr.js
@@ -2,13 +2,14 @@ console.log('flat arr');
 
 function flat_arr(arr, depth = 1) {
   if (depth > 0) {
-    return arr.reduce((p, x) => {
-      return p.concat(Array.isArray(x) ? flat_arr(x, depth - 1) : x);
+    return arr.reduce((p, arr) => {
+      return p.concat(Array.isArray(arr) ? flat_arr(arr, depth - 1) : arr);
     }, []);
   }
 
+  // 浅拷贝
   return arr.slice();
 }
 
 const arr = [1, [2, 3], [4, 5], [6, 7, [8, 9]]];
-console.log(flat_arr(arr, 2));
+console.log(flat_arr(arr, 1));
