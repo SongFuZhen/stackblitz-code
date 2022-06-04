58b6825ad31b423c598713fda1dc464d418363d0
SongFuZhen <evil_song@126.com>
2022-06-04 16:44:14 +0800

    1 file modified

--- algorithm/sort/bubbleSort.js
+++ algorithm/sort/bubbleSort.js
@@ -1,2 +1,21 @@
 console.log('冒泡排序');
 
+// 原理: 相邻两元素之间两两比较，比较出大值进行赋值互换，再依次与相邻的元素比较
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+let arr = [1, 2, 34, 5, 3, 3, 9, 10];
+
+console.log(bubbleSort(arr));
