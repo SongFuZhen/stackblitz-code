ec41e408fe2e86a9a5711f5d52b0dae9b8eafd3f
SongFuZhen <evil_song@126.com>
2022-06-07 11:51:40 +0800

    1 file modified

--- algorithm/sort/quickSort.js
+++ algorithm/sort/quickSort.js
@@ -6,3 +6,29 @@ console.log('快速排序');
  * 2. 遍历数组，大于的放数组右边，小于的放在左边
  * 3. 递归并且用 concat 连接
  */
+
+function quickSort(arr) {
+  if (arr.length < 1) {
+    return arr;
+  }
+
+  // 取出中间值
+  let num = Math.floor(arr.length / 2);
+  let idx = arr.splice(num, 1)[0];
+
+  let left = [];
+  let right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < idx) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return quickSort(left).concat(idx, quickSort(right));
+}
+
+const arr = [1, 32, 4, 5, 23, 22, 12, 13, 65, 77, 01];
+console.log(quickSort(arr));
