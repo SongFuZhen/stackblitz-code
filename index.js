884f27104b481a234ad4920ab6aac7be087dae11
SongFuZhen <evil_song@126.com>
2022-06-08 15:10:09 +0800

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Javascript Algorithm</h1>`;
+
+// 排序
+import './sort/bubble.js';
