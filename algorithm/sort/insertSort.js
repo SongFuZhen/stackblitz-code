178574bdc4d62b6d831c60b11ffcd88137de90bf
SongFuZhen <evil_song@126.com>
2022-06-08 10:03:45 +0800

    1 file modified

--- algorithm/sort/insertSort.js
+++ algorithm/sort/insertSort.js
@@ -5,21 +5,28 @@ console.log('插入排序');
  * 通过循环想要排序的数组
  * 并和新数组的值从后向前比较
  * 大于就splice到该项后面
+ * 如果 到 0 还没有比新数组大到，那么就添加在头部
  */
 
 function insertSort(arr) {
-  // const num = Math.floor(arr.length / 2);
-  // const newArr = arr.splice(num, 1);
-  // for (let i = 0; i < arr.length; i++) {
-  //   for (let j = newArr.length - 1; j <= 0; j--) {
-  //     if (arr[i] > newArr[j]) {
-  //       newArr.splice(j, 1, arr[i]);
-  //     }
-  //   }
-  // }
-  // return newArr;
+  let newArr = [];
+  newArr.push(arr[0]);
 
-  return arr;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = newArr.length - 1; j >= 0; j--) {
+      if (arr[i] > newArr[j]) {
+        newArr.splice(j + 1, 0, arr[i]);
+        break;
+      }
+
+      // 重点
+      if (j === 0) {
+        newArr.unshift(arr[i]);
+      }
+    }
+  }
+
+  return newArr;
 }
 
 const arr = [1, 2, 4, 6, 6, 712, 1212, 312, 22, 3, 45];
