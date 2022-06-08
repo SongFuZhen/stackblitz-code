7eb37dbece21213e527b03f05f8c9965a6b312b8
SongFuZhen <evil_song@126.com>
2022-06-08 09:49:34 +0800

    1 file modified

--- algorithm/sort/insertSort.js
+++ algorithm/sort/insertSort.js
@@ -1,3 +1,5 @@
+console.log('插入排序');
+
 /**
  * 拿出一个值，作为初始数组的值，
  * 通过循环想要排序的数组
@@ -5,22 +7,20 @@
  * 大于就splice到该项后面
  */
 
-console.log('InsertSort');
+function insertSort(arr) {
+  // const num = Math.floor(arr.length / 2);
+  // const newArr = arr.splice(num, 1);
+  // for (let i = 0; i < arr.length; i++) {
+  //   for (let j = newArr.length - 1; j <= 0; j--) {
+  //     if (arr[i] > newArr[j]) {
+  //       newArr.splice(j, 1, arr[i]);
+  //     }
+  //   }
+  // }
+  // return newArr;
 
-// function insertSort(arr) {
-//   const num = Math.floor(arr.length / 2);
-//   const newArr = arr.splice(num, 1);
+  return arr;
+}
 
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = newArr.length - 1; j <= 0; j--) {
-//       if (arr[i] > newArr[j]) {
-//         newArr.splice(j, 1, arr[i]);
-//       }
-//     }
-//   }
-
-//   return newArr;
-// }
-
-// const arr = [1, 2, 4, 6, 6, 712, 1212, 312, 22, 3, 45];
-// console.log(insertSort(arr));
+const arr = [1, 2, 4, 6, 6, 712, 1212, 312, 22, 3, 45];
+console.log(insertSort(arr));
