4e3ec523d7cca6f348f9b3e038a53a6baa336de7
SongFuZhen <Evil_song@126.com>
2022-10-05 16:28:58 +0800

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// https://juejin.cn/post/7023906112843808804#heading-1
+// JS 代码手写 50 例
